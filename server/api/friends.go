33eaae2a2c9d635a7c76542d1f64c9fe1a02c25c
aj <aj_funelas@yahoo.com>
2020-12-29 17:33:43 +0800

    ListFriends [WIP]

--- server/api/friends.go
+++ server/api/friends.go
@@ -45,97 +45,23 @@ type FriendsListRequest struct {
 
 // OpportunityMany return a list of opportunities
 func (c *FriendController) ListFriends(w http.ResponseWriter, r *http.Request, u *db.User) (int, error) {
-	// 	req := &FriendsListRequest{}
-	// 	err := json.NewDecoder(r.Body).Decode(req)
-	// 	if err != nil {
-	// 		return http.StatusBadRequest, terror.New(err, "invalid input")
-	// 	}
-	// 	defer r.Body.Close()
+	// req := &FriendsListRequest{}
+	// err := json.NewDecoder(r.Body).Decode(req)
+	// if err != nil {
+	// 	return http.StatusBadRequest, terror.New(err, "invalid input")
+	// }
+	// defer r.Body.Close()
 
-	// 	search := req.Search
-	// 	limit := req.Limit
-	// 	offset := req.Offset
+	// search := req.Search
+	// limit := req.Limit
+	// offset := req.Offset
 
-	// 	queries := []qm.QueryMod{
-	// 		qm.Load(
-	// 			db.OpportunityRels.Users,
-	// 		),
-	// 		qm.Load(
-	// 			qm.Rels(
-	// 				db.OpportunityRels.Business,
-	// 				db.BusinessRels.Owner,
-	// 			),
-	// 		),
-	// 	}
-
-	// 	// join owner table
-	// 	queries = append(queries,
-	// 		qm.InnerJoin(
-	// 			fmt.Sprintf(
-	// 				"%[1]s ON %[1]s.%[2]s = %[3]s.%[4]s",
-	// 				db.TableNames.Businesses,
-	// 				db.BusinessColumns.ID,
-	// 				db.TableNames.Opportunities,
-	// 				db.OpportunityColumns.BusinessID,
-	// 			),
-	// 		),
-	// 		qm.InnerJoin(
-	// 			fmt.Sprintf(
-	// 				"%[1]s ON %[1]s.%[2]s = %[3]s.%[4]s",
-	// 				db.TableNames.Users,
-	// 				db.UserColumns.ID,
-	// 				db.TableNames.Businesses,
-	// 				db.BusinessColumns.OwnerID,
-	// 			),
-	// 		),
-	// 	)
-
-	// 	// Search
-	// 	if search != "" {
-	// 		searchKey := helpers.ParseQueryText(search)
-	// 		if len(searchKey) > 0 {
-	// 			queries = append(queries,
-	// 				qm.Where(
-	// 					fmt.Sprintf("(coalesce(%[1]s.%[3]s,'')) ||  (coalesce(%[2]s.%[3]s,'')) @@ to_tsquery( ? )",
-	// 						db.TableNames.Users,
-	// 						db.TableNames.Opportunities,
-	// 						db.OpportunityColumns.Keywords,
-	// 					),
-	// 					searchKey,
-	// 				),
-	// 			)
-	// 		}
-	// 	}
-
-	// 	// Filter
-	// 	if req.Filter.IsValid() {
-	// 		switch *req.Filter {
-	// 		case CategoryTypeMoblie:
-	// 		case CategoryTypeSocialMedia:
-	// 		case CategoryTypeWebsite:
-	// 		}
-	// 	}
-
-	// 	count, err := db.Opportunities(queries...).Count(c.Conn)
-	// 	if err != nil {
-	// 		return http.StatusInternalServerError, terror.New(err, "get many opportunities")
-	// 	}
-
-	// 	queries = append(queries, qm.Limit(limit), qm.Offset(offset))
-	// 	os, err := db.Opportunities(queries...).All(c.Conn)
-	// 	if err != nil {
-	// 		return http.StatusInternalServerError, terror.New(err, "get many opportunities")
-	// 	}
-
-	// 	resp := &OpportunitesManyResponse{
-	// 		Total: int(count),
-	// 	}
-	// 	for _, o := range os {
-	// 		newOp := NewOpportunity(o, c.BlobURL)
-	// 		newOp.IsFave = findIDInUserList(u.ID, o.R.Users)
-	// 		resp.Opportunites = append(resp.Opportunites, newOp)
-	// 	}
-
-	// 	return helpers.EncodeJSON(w, resp)
+	// queries := []qm.QueryMod{
+	// 	qm.Load(
+	// 		db.ConnectionRels.Users,
+	// 	),
+	// }
 	panic("")
 }
+
+// u.Connections().All(c.Conn)
