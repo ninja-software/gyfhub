452371e8ad2b3f5cb7ec8c7663120557def3cbb1
owengiri20 <owengiri20@gmail.com>
2020-12-29 12:06:12 +0800

    front end chat

--- web/src/pages/chat/chat.tsx
+++ web/src/pages/chat/chat.tsx
@@ -1,42 +1,22 @@
-import * as React from "react"
 import { makeStyles, TextField, Typography } from "@material-ui/core"
+import * as React from "react"
 import { ExpButton } from "../../components/common/button"
-import { check } from "prettier"
+import { UserAvatar } from "../../components/common/avatar"
+import { AuthContainer } from "../../controllers/auth"
+import { GifObject } from "../../types/types"
 
 interface Props {}
-
-export interface IGifImage {
-	url: string
-	height: string
-	width: string
-}
-
-export enum Rating {
-	Y = "y",
-	G = "g",
-	PG = "pg",
-	PG13 = "pg-13",
-	R = "r",
-}
-
-export interface IGifObject {
-	id: string
-	slug: string
-	url: string
-	embed_url: string
-	source: string
-	rating: Rating
-	title: string
-	images: { [index: string]: IGifImage }
-}
 
 const useStyle = makeStyles((theme) => ({
 	container: {
 		height: "90%",
 	},
-	gifsContainer: {
+	messagesContainer: {
 		height: "50%",
 		border: "1px solid black",
+		overflowY: "auto",
+		paddingTop: "20px",
+		paddingLeft: "20px",
 	},
 	keyboardContainer: {
 		marginTop: "15px",
@@ -54,7 +34,22 @@ const useStyle = makeStyles((theme) => ({
 	},
 
 	gifImage: {
-		width: "160px",
+		width: "170px",
+	},
+
+	messageContainer: {
+		display: "flex",
+		alignItems: "flex-start",
+		marginTop: "20px",
+		marginBottom: "20px",
+	},
+
+	messageImage: {
+		width: "60%",
+	},
+
+	avatarContainer: {
+		margin: "20px",
 	},
 
 	searchBar: {
@@ -67,12 +62,17 @@ const useStyle = makeStyles((theme) => ({
 export const Chat = (props: Props) => {
 	const {} = props
 
-	const [searchResults, setSearchResults] = React.useState<IGifObject[]>([])
+	const apiKey = "iKnyHPF6aER2DrPWjQGdgHS9O1oksVFv&q" // todo move this
+
+	const { currentUser } = AuthContainer.useContainer()
+	const [searchResults, setSearchResults] = React.useState<GifObject[]>([])
 	const [searchQuery, setSearchQuery] = React.useState<string>("")
+
+	const [messages, setMessages] = React.useState<string[]>([])
 
 	const getSearchResults = async () => {
 		setSearchResults([])
-		return await fetch(`https://api.giphy.com/v1/gifs/search?api_key=iKnyHPF6aER2DrPWjQGdgHS9O1oksVFv&q=${searchQuery}&limit=60&offset=0&rating=R&lang=en`)
+		return await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${searchQuery}&limit=60&offset=0&rating=R&lang=en`)
 			.then((res) => {
 				return res.json()
 			})
@@ -81,13 +81,30 @@ export const Chat = (props: Props) => {
 			})
 	}
 
+	// todo change later
+	const addMessage = (msg: string) => {
+		setMessages([...messages, msg])
+	}
+
 	const classes = useStyle()
 
-	console.log("gifs here ========================", searchResults)
-
+	if (!currentUser) {
+		return <div>an error occurred</div>
+	}
 	return (
 		<div className={classes.container}>
-			<div className={classes.gifsContainer}>gifs goes here</div>
+			<div className={classes.messagesContainer}>
+				{messages.map((m) => {
+					return (
+						<div className={classes.messageContainer}>
+							<div className={classes.avatarContainer}>
+								<UserAvatar size={70} {...currentUser} />
+							</div>
+							<img className={classes.messageImage} src={m} alt="" />
+						</div>
+					)
+				})}
+			</div>
 
 			<div className={classes.keyboardContainer}>
 				<div className={classes.searchBar}>
@@ -111,7 +128,7 @@ export const Chat = (props: Props) => {
 				<div className={classes.gifsGrid}>
 					{searchResults.map((s) => {
 						return (
-							<div className={classes.gifImageContainer}>
+							<div className={classes.gifImageContainer} onClick={() => addMessage(s.images.downsized.url)}>
 								<img className={classes.gifImage} src={s.images.downsized.url} alt="" />
 							</div>
 						)
