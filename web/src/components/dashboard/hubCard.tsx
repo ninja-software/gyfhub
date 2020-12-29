2829911ceaedbfb881eed7b10f530c259eacdb40
owengiri20 <owengiri20@gmail.com>
2020-12-29 16:32:18 +0800

    stuffs

--- web/src/components/dashboard/hubCard.tsx
+++ web/src/components/dashboard/hubCard.tsx
@@ -1,15 +1,24 @@
 import * as React from "react"
 import { ExpCard } from "../common/card"
-import { Typography, Box } from "@material-ui/core"
+import { Typography, Box, makeStyles } from "@material-ui/core"
 import { useHistory } from "react-router-dom"
 import { ExpButton } from "../common/button"
 import { useQuery } from "react-fetching-library"
 import { fetching } from "../../fetching"
 import { Hub } from "../../types/types"
 
+const useStyle = makeStyles((theme) => ({
+	hubList: {
+		display: "flex",
+		flexDirection: "column",
+	},
+}))
+
 interface Props {}
 export const HubCard = (props: Props) => {
 	const {} = props
+
+	const classes = useStyle()
 
 	const history = useHistory()
 
@@ -30,13 +39,19 @@ export const HubCard = (props: Props) => {
 				</div>
 			</div>
 
-			<div>
+			<div className={classes.hubList}>
 				{/* // todo make this look pretty */}
-				<Box m={10} />
+				<Box m={5} />
 
 				<div>list here</div>
 				{data?.map((d) => {
-					return <div key={d.id}>{d.name}</div>
+					return (
+						<div style={{ marginTop: "10px" }}>
+							<ExpButton onClick={() => history.push("/hubs/" + d.id)} key={d.id}>
+								hub name: {d.name}
+							</ExpButton>
+						</div>
+					)
 				})}
 			</div>
 		</ExpCard>
