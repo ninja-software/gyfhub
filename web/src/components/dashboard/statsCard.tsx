77b6918a855fee865c5224aa6752086718e5ad66
owengiri20 <owengiri20@gmail.com>
2020-12-30 11:04:44 +0800

    stats dashboard frontend

--- web/src/components/dashboard/statsCard.tsx
+++ web/src/components/dashboard/statsCard.tsx
@@ -8,28 +8,42 @@ import { fetching } from "../../fetching"
 import { Hub } from "../../types/types"
 
 const useStyle = makeStyles((theme) => ({
-	hubList: {
+	statsList: {
 		display: "flex",
+		flexDirection: "column",
 	},
 
 	top: {
 		display: "flex",
 		justifyContent: "space-between",
+		marginTop: "20px",
 	},
 	hubBtn: {
-		height: "100px",
-		width: "100px",
+		height: "200px",
+		width: "250px",
 		marginTop: "10px",
-		marginRight: "10px",
-		border: "1px solid black",
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+
+	statRow: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingBottom: "10px",
+		marginBottom: "10px",
+		borderBottom: "1px solid grey",
 	},
 	viewAllBtn: {
 		display: "flex",
 		justifyContent: "center",
 		alignItems: "center",
+	},
+
+	gifImage: {
+		width: "100%",
+		height: "100%",
 	},
 }))
 
@@ -37,14 +51,8 @@ export const StatsCard = () => {
 	const classes = useStyle()
 	const history = useHistory()
 
-	const [hubs, setHubs] = React.useState<Hub[]>([])
-
+	// todo get data
 	const { payload: data, loading, error } = useQuery<Hub[]>(fetching.queries.allHubs())
-
-	React.useEffect(() => {
-		if (loading || error || !data) return
-		setHubs(data)
-	}, [data])
 
 	if (!loading && error) return <div>An error occurred</div>
 
@@ -53,34 +61,66 @@ export const StatsCard = () => {
 			<div className={classes.top}>
 				<div>
 					<Typography variant="h1">
-						<Box fontWeight="bold">Stats</Box>
+						<Box fontWeight="bold">My Stats</Box>
 					</Typography>
 				</div>
 			</div>
 
-			<div className={classes.hubList}>
-				{/* // todo make this look pretty */}
-				{hubs.map((d, idx) => {
-					// render the first 3
-					if (idx >= 3) {
-						return <></>
-					}
-					return (
-						<div
-							className={classes.hubBtn}
-							onClick={() => {
-								// todo refactor this
-								history.push("/chat")
-							}}
-						>
-							{/* todo change to render avatar instead of initials */}
-							<Typography variant="h2">{d.name[0].toUpperCase()}</Typography>
-						</div>
-					)
-				})}
+			<div className={classes.statsList}>
+				<div className={classes.statRow}>
+					<div>
+						<Typography variant="h3"> Most Used</Typography>
+					</div>
+
+					<div className={classes.hubBtn}>
+						<img className={classes.gifImage} src="https://giffiles.alphacoders.com/130/13036.gif" alt="" />
+					</div>
+				</div>
+
+				<div className={classes.statRow}>
+					<div>
+						<Typography variant="h3"> Gifs Sent</Typography>
+					</div>
+
+					<div className={classes.hubBtn}>
+						<Typography variant="h1">
+							<Box fontWeight="bold">50</Box>
+						</Typography>
+					</div>
+				</div>
+			</div>
+
+			<div className={classes.top}>
+				<div>
+					<Typography variant="h1">
+						<Box fontWeight="bold">Global Stats</Box>
+					</Typography>
+				</div>
+			</div>
+
+			<div className={classes.statsList}>
+				<div className={classes.statRow}>
+					<div>
+						<Typography variant="h3"> Most popular</Typography>
+					</div>
+
+					<div className={classes.hubBtn}>
+						<img className={classes.gifImage} src="https://giffiles.alphacoders.com/130/13036.gif" alt="" />
+					</div>
+				</div>
+
+				<div className={classes.statRow}>
+					<div>
+						<Typography variant="h3">Trending</Typography>
+					</div>
+
+					<div className={classes.hubBtn}>
+						<img className={classes.gifImage} src="https://media1.tenor.com/images/d3e70b1dab8d316aab7406f9e242190b/tenor.gif?itemid=15610659" alt="" />
+					</div>
+				</div>
 
 				<div className={classes.viewAllBtn}>
-					<ExpButton styleType={"tertiary"}>View all hubs</ExpButton>
+					<ExpButton styleType={"tertiary"}>View all</ExpButton>
 				</div>
 			</div>
 		</ExpCard>
